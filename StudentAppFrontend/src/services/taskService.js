f849f423866ed71139463dc18cca2356bd68f415
Szuum02 <s.urbanski2002@gmail.com>
2026-05-22 23:08:45 +0200

    Retreive tasks from database

--- StudentAppFrontend/src/services/taskService.js
+++ StudentAppFrontend/src/services/taskService.js
@@ -1,70 +1,25 @@
 const API_URL = "http://localhost:5174/api";
 
 export const getTasks = async () => {
-  const response = await fetch(API_URL);
+  const url = API_URL + "/tasks";
+  const token = localStorage.getItem("token");
 
-  if (!response.ok) {
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
     throw new Error("Cannot get task from database");
   }
-  return [
-    {
-      id: 1,
-      title: "Zrobić projekt React",
-      date: "2026-05-12",
-      description: "Dokończyć dashboard aplikacji To-Do List.",
-      completed: false,
-    },
-    {
-      id: 2,
-      title: "Nauka CSS",
-      date: "2026-05-15",
-      description: "Powtórzyć flexbox oraz grid.",
-      completed: true,
-    },
-    {
-      id: 3,
-      title: "Oddać dokumentację",
-      date: "2026-05-01",
-      description: "Wysłać dokumentację projektu.",
-      completed: false,
-    },
-    {
-      id: 4,
-      title: "Nowe zadanie",
-      date: "2026-05-01",
-      description: "Coś tam coś tam",
-      completed: true,
-    },
-    {
-      id: 5,
-      title: "Nowe zadanie",
-      date: "2026-05-01",
-      description:
-        "Coś tam coś tam Coś tam coś tamCoś tam coś tam vCoś tam coś tamCoś tam coś tam  Coś tam coś tamCoś tam coś tam ",
-      completed: false,
-    },
-    {
-      id: 6,
-      title: "Nowe zadanie",
-      date: "2026-05-01",
-      description: "Coś tam coś tam",
-      completed: false,
-    },
-    {
-      id: 7,
-      title: "Nowe zadanie",
-      date: "2026-05-01",
-      description: "Coś tam coś tam",
-      completed: false,
-    },
-  ];
-  //   return response.json();
+  return await res.json();
 };
 
 export const createTask = async (taskData) => {
   const url = API_URL + "/tasks";
   const token = localStorage.getItem("token");
-  console.log(token);
 
   const dto = {
     title: taskData.title,
